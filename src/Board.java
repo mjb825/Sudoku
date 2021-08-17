725ba46817422f72d039e8c609d0d729c5e45b62
Matthew Below <55146074+mjbelow@users.noreply.github.com>
2021-08-17 11:29:06 -0400

    Containerize Samurai Sudoku

--- src/Board.java
+++ src/Board.java
@@ -15,30 +15,50 @@ import javafx.scene.input.KeyCode;
 public class Board extends GridPane
 {
 	
-	Cell[][] cell;
+	Cell[][][] cell;
 	Sudoku sudoku;
 	
-	public Board(Sudoku sudoku, int puzzle[][])
+	public Board(Sudoku sudoku, int puzzle[][][])
 	{
 		this.sudoku = sudoku;
-		cell = new Cell[puzzle.length][puzzle[0].length];
-	
-		for(int row = 0; row < puzzle.length; row++) {
+		cell = new Cell[puzzle.length][puzzle[0].length][puzzle[0][0].length];
+		
+		for(int sec = 0; sec < puzzle.length; sec++) {
+		// for(int sec = 4; sec >= 0; sec--) {
 			
-			for(int col = 0; col < puzzle[row].length; col++) {
+			for(int row = 0; row < puzzle[sec].length; row++) {
 				
-				cell[row][col] = new Cell(puzzle[row][col]);
+				for(int col = 0; col < puzzle[sec][row].length; col++) {
+					
+					cell[sec][row][col] = new Cell(puzzle[sec][row][col], sec, 3 * (row / 3) + (col / 3));
+					
+					// if(sec == 0)
+						// cell[sec][row][col].setColor(Color.rgb(200, 255, 200));
+					// else 
+					if(  ((col / 3 == 0 || col / 3 == 2) && (row / 3 == 0 || row / 3 == 2)) || ((col / 3 == 1) && (row / 3 == 1)) )
+						cell[sec][row][col].setColor(Color.rgb(255, 255, 200));
+					else
+						cell[sec][row][col].setColor(Color.WHITE);
+					
+					
+					
+					if(sec == 0)
+						add(cell[sec][row][col], col, row);
+					else if(sec == 1)
+						add(cell[sec][row][col], col + 9 + 3, row);
+					else if(sec == 2)
+						add(cell[sec][row][col], col, row + 9 + 3);
+					else if(sec == 3)
+						add(cell[sec][row][col], col + 9 + 3, row + 9 + 3);
+					else if(sec == 4)
+						add(cell[sec][row][col], col + 6, row + 6);
+					
+				}
 				
-				if(  ((col / 3 == 0 || col / 3 == 2) && (row / 3 == 0 || row / 3 == 2)) || ((col / 3 == 1) && (row / 3 == 1)) )
-					cell[row][col].setColor(Color.rgb(255, 255, 200));
-				else
-					cell[row][col].setColor(Color.WHITE);
-				
-				add(cell[row][col], col, row);
 			}
 			
-		}	
-	
+		}
+
 	}
 	
 
@@ -56,19 +76,22 @@ public class Board extends GridPane
 			
 		}
 		
-		public Cell(int n)
+		public Cell(int n, int sec, int sqr)
 		{
 			
 			number = n;
-			setFocusTraversable(true);
-			setOnMouseClicked(e -> requestFocus());
+			if(   (sec == 0 && sqr == 8)   ||   (sec == 1 && sqr == 6)   ||   (sec == 2 && sqr == 2)   |   (sec == 3 && sqr == 0)   )
+				setFocusTraversable(false);
+			else
+				setFocusTraversable(true);
+			setOnMousePressed(e -> requestFocus());
 			
-			box = new Rectangle(40, 40);
+			box = new Rectangle(30, 30);
 			
 			box.setStyle("-fx-stroke: black; -fx-stroke-width: 1;");
 			
-			
 			num = new Label(String.valueOf(n));
+			num.setStyle("-fx-font: bold 20 arial;");
 			num.setTextFill(Color.BLACK);
 			
 			
@@ -85,7 +108,6 @@ public class Board extends GridPane
 				}
 				else if(code == KeyCode.R && e.isControlDown())
 					reset();
-				// System.out.println(TF.getCharacters());
 						
 			});
 			
@@ -171,32 +193,62 @@ public class Board extends GridPane
 	
 	public void reset() {
 		
-		for(int row = 0; row < cell.length; row++)
-			for(int col = 0; col < cell[row].length; col++)
-				cell[row][col].setNumber(0);
+		for(int sec = 0; sec < cell.length; sec++)
+			for(int row = 0; row < cell[sec].length; row++)
+				for(int col = 0; col < cell[sec][row].length; col++)
+					cell[sec][row][col].setNumber(0);
 		
 		
 	}
 	
-	public int[][] getPuzzle() {
+	public int[][][] getPuzzle() {
 		
-		int[][] userPuzzle = new int[cell.length][cell[0].length];
+		int[][][] userPuzzle = new int[cell.length][cell[0].length][cell[0][0].length];
 		
-		for(int row = 0; row < cell.length; row++)
-			for(int col = 0; col < cell[row].length; col++)
-				userPuzzle[row][col] = cell[row][col].getNumber();
+		int sqr;
+		
+		for(int sec = 0; sec < cell.length; sec++)
+			for(int row = 0; row < cell[sec].length; row++)
+				for(int col = 0; col < cell[sec][row].length; col++) {
+					
+					sqr = 3 * (row / 3) + (col / 3);
+					
+					// assign all the cells to userPuzzle
+					// if the cells were hidden by the center puzzle, get the values from the center puzzle
+					if(sec == 0 && sqr == 8)
+					{	userPuzzle[sec][row][col] = cell[4][row-6][col-6].getNumber();
+						// System.out.println(sec + "0 8" + sqr + ": " + cell[4][row-6][col-6].getNumber());
+					}
+					else if(sec == 1 && sqr == 6)
+					{	userPuzzle[sec][row][col] = cell[4][row-6][col+6].getNumber();
+						// System.out.println(sec + "1 6" + sqr + ": " + cell[4][row-6][col+6].getNumber());
+					}
+					else if(sec == 2 && sqr == 2)
+					{	userPuzzle[sec][row][col] = cell[4][row+6][col-6].getNumber();
+						// System.out.println(sec + "2 2" + sqr + ": " + cell[4][row+6][col-6].getNumber());
+					}
+					else if(sec == 3 && sqr == 0)
+					{	userPuzzle[sec][row][col] = cell[4][row+6][col+6].getNumber();
+						// System.out.println(sec + "3 0" + sqr + ": " + cell[4][row+6][col+6].getNumber());
+					}
+					else
+						userPuzzle[sec][row][col] = cell[sec][row][col].getNumber();
+					
+					
+					
+				}
 		
 		return userPuzzle;
 		
 	}
 	
-	public void showSolution(int[][] puzzle) {
+	public void showSolution(int[][][] puzzle) {
 		
-		for(int row = 0; row < puzzle.length; row++)
-			for(int col = 0; col < puzzle[row].length; col++)
-				cell[row][col].setNumber(puzzle[row][col]);
+		for(int sec = 0; sec < puzzle.length; sec++)
+			for(int row = 0; row < puzzle[sec].length; row++)
+				for(int col = 0; col < puzzle[sec][row].length; col++)
+					cell[sec][row][col].setNumber(puzzle[sec][row][col]);
 		
 		
-	}
-	
+	}	
 }
